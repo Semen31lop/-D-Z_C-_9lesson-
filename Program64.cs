8dc18d0af64f466ce29d2d969d6a5f55546585c7
Semen31lop <119083691+Semen31lop@users.noreply.github.com>
2022-12-25 21:54:59 +0300

    Add files via upload

---
+++ Program64.cs
@@ -0,0 +1,18 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write("Введите занчение : ");
+int num = int.Parse(Console.ReadLine()!);
+string NaturalNum(int start , int end)
+{
+if (start==end)
+{
+    return start.ToString();
+}
+return start+"  " +NaturalNum(start+1,end);
+}
+Console.Write(NaturalNum(1,num));
+
+
+
---
+++ Program66.cs
@@ -0,0 +1,16 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.Write("Введите занчение : ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write("Введите занчение : ");
+int M = int.Parse(Console.ReadLine()!);
+
+int SumNum(int start , int end)
+{
+    if (start == end)
+        return end;
+    return start + "  " + SumNum(start , end-1);
+}
+Console.Write(SumNum(N,M));
