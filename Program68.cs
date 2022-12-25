f2218e83e30e31d8fb99fd7453d99b5274e6a4a3
Semen31lop <119083691+Semen31lop@users.noreply.github.com>
2022-12-25 22:02:06 +0300

    Add files via upload

---
+++ Program68.cs
@@ -0,0 +1,25 @@
+﻿// Очень странная функция
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+AkkermanFunction(m,n);
